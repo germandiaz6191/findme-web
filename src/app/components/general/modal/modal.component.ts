c52b33593b23abdd81b842f2d55bef9f9c22417d
Germán Dario Gallego <aksa7893@gmail.com>
2024-02-26 15:29:31 +0000

    Updated 4 files under /src/app/components/general/modal

---
+++ src/app/components/general/modal/modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  @Input() title;
+  @Input() content;
+
+  constructor(private activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+  dismiss(cont: any){
+    this.activeModal.dismiss(cont);
+  }
+  
+
+}
