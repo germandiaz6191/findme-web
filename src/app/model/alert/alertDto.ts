1aa934d60cc49957b47a5e48c79ac6a9971f647b
Germán Dario Gallego <aksa7893@gmail.com>
2024-02-26 16:15:26 +0000

    Updated alertDto.ts

---
+++ src/app/model/alert/alertDto.ts
@@ -0,0 +1,14 @@
+/**
+ * DTO to Show the alert
+ * 
+ * @author German
+ * 
+ */
+ export class AlertDto {
+
+	public title: String;
+	public content: String;
+	public navigatorGo: String;
+	public navigatorBack: String;
+	
+}
