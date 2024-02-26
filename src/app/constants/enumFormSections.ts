c87072394e3aae1d32742f3efa0fd05bfac7ac46
Germán Dario Gallego <aksa7893@gmail.com>
2024-02-26 16:14:08 +0000

    Updated enumFormSections.ts

---
+++ src/app/constants/enumFormSections.ts
@@ -0,0 +1,6 @@
+export enum EnumFormSections {
+    altertAddSector = "alert-add-sector",
+    addSector = "app-add-sector",
+    addAditionalInformation = "app-add-aditional-information",
+    changeStatus = "app-change-status-object"
+  }
