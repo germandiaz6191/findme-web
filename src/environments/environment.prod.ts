9093b6957b4ee5acaac291f327c97668a27ff942
Germán Dario Gallego <aksa7893@gmail.com>
2024-02-26 15:20:01 +0000

    Updated 2 files under /src/environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  
+  //QA
+  baseUrlLogin: 'https://findme-gateway.herokuapp.com/out/initial/',
+  baseUrlGeneral: 'https://findme-gateway.herokuapp.com/auth/general/',
+  baseUrlAuthenticated: 'https://app-findme-front.herokuapp.com/',
+  homePage: '#/home/',
+
+  baseUrlIp: 'https://jsonip.com',
+  versionTermsnConditions: '1'
+};
